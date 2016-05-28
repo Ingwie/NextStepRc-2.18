31c90ed8b47ca6e29f68acf1dceac5c2076df0bc
Ingwie <Ingwie@users.noreply.github.com>
2016-05-28 11:58:21 +0200

    Rebuild broken code, add separate Lcd drivers

---
+++ radio/util/clean_ifdef/tri.bat
@@ -0,0 +1,5 @@
+sunifdef --recurse --filter c,cpp,h,txt --keepgoing --undef CPUARM A:\radio\src
+sunifdef --recurse --filter c,cpp,h,txt --keepgoing --undef PCBTARANIS A:\radio\src
+sunifdef --recurse --filter c,cpp,h,txt --keepgoing --undef PCBSKY9X A:\radio\src
+del sunifde*.* /s
+pause
